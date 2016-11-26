4e44cd2f5d997d522286bdd1c9c40aa074419de7
Jack <mtjeck@gmail.com>
2016-11-26 09:42:37 +0300

    update code: add yield, edit time-operations, correct var-names

--- seek_dev_nighters.py
+++ seek_dev_nighters.py
@@ -5,45 +5,45 @@ from datetime import datetime
 
 
 def load_attempts():
-    data = []
     page_number = 0
     while 1:
         page_number += 1
-        url = 'http://devman.org/api/challenges/solution_attempts/?page=%s' % page_number
-        r = requests.get(url)
+        payload = {'page': page_number}
+        r = requests.get('http://devman.org/api/challenges/solution_attempts/', params=payload)
         if r.status_code == 200:
-            page_data = json.loads(r.text)
-            data += page_data['records']
+            data_page = json.loads(r.text)
+            yield data_page['records']
         else:
             break
-    return data
 
 
-def get_midnighters(data):
+def get_midnighters(solved_tasks):
     fmt = '%H:%M:%S'
     users = set()
-    for action in data:
-        username = action['username']
-        timestamp = action['timestamp']
-        timezone = action['timezone']
+    for task in solved_tasks:
+        username = task['username']
+        timestamp = task['timestamp']
+        timezone = task['timezone']
         if timestamp:
             utc_dt = datetime.utcfromtimestamp(timestamp).replace(tzinfo=pytz.utc)
             user_tz = pytz.timezone(timezone)
             user_dt = user_tz.normalize(utc_dt.astimezone(user_tz))
-            upload_time = datetime.strptime(user_dt.strftime(fmt), "%H:%M:%S").time()
-            midnight = datetime.strptime('0:0:0', "%H:%M:%S").time()
-            morning = datetime.strptime('6:0:0', "%H:%M:%S").time()
-            if midnight < upload_time < morning:
+            upload_time = user_dt.time()
+            midnight = datetime.strptime('0:0:0', fmt).time()
+            morning = datetime.strptime('6:0:0', fmt).time()
+            if midnight <= upload_time < morning:
                 users.add(username)
     return users
 
 
-def print_midnighters(users):
-    for user in sorted(users):
-        print(user)
+def print_midnighters(owls):
+    print("Night owls on DevMan.org:")
+    for i, owl in enumerate(sorted(owls)):
+        print("%s) %s" % (i+1, owl))
 
 
 if __name__ == '__main__':
-    data = load_attempts()
-    midnighters = get_midnighters(data)
-    print_midnighters(midnighters)
+    api_data = []
+    for x in load_attempts():
+        api_data += x
+    print_midnighters(get_midnighters(solved_tasks=api_data))
